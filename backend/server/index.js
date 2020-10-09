7906e504d987f1f53a33fb595d2ed00fea633750
IbrahemAtef <bulbul.20001@gmail.com>
2020-10-09 17:23:30 +0300

    new structure

--- backend/server/index.js
+++ backend/server/index.js
@@ -6,4 +6,13 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
