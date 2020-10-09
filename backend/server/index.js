dcb8265e3d88015474e12e00064db3fa3157b74d
IbrahemAtef <bulbul.20001@gmail.com>
2020-10-09 18:20:19 +0300

    back done

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,18 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+app.use(express.json({ extended: false }));
 
-// app.get('/', (req, res) => res.send('API Running'));
+app.get('/test', (req, res) =>res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
-
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
-  });
-}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
